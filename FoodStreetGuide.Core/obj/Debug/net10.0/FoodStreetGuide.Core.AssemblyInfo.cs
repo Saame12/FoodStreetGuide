320fbb9ee1eebc603d64cb7669344171747321a9
Sang Lê <Sangle20050627@gmail.com>
2026-04-02 09:40:38 +0700

    comit

--- FoodStreetGuide.Core/obj/Debug/net10.0/FoodStreetGuide.Core.AssemblyInfo.cs
+++ FoodStreetGuide.Core/obj/Debug/net10.0/FoodStreetGuide.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodStreetGuide.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4467383c87c6fd71bc607b8dea34c7dc200d0a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodStreetGuide.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodStreetGuide.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
